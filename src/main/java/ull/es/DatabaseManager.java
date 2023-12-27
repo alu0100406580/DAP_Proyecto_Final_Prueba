bae038797609e3737bc4eebd91bf125b2830db74
Dario Afonso <alu0100406580@ull.edu.es>
2023-12-27 22:47:07 +0000

    Arreglo insercion historico y no repeticion productos ya agregados

--- src/main/java/ull/es/DatabaseManager.java
+++ src/main/java/ull/es/DatabaseManager.java
@@ -7,7 +7,8 @@ public class DatabaseManager {
 
     private final String databasePath = "database/supermarketProducts.db";
 
-    public DatabaseManager() {}
+    public DatabaseManager() {
+    }
 
     public Connection connect() throws SQLException {
         try {
@@ -38,30 +39,49 @@ public class DatabaseManager {
 
             String insertProductQuery = "INSERT INTO Products (id_by_supermarket, id_supermarket, name, price, imageUrl, urlProduct) VALUES (?, ?, ?, ?, ?, ?)";
             String insertPriceHistoryQuery = "INSERT INTO PriceHistory (productId, price) VALUES (?, ?)";
+            String checkExistingProductQuery = "SELECT COUNT(*) FROM Products WHERE id_by_supermarket = ? AND id_supermarket = ?";
 
             try (PreparedStatement insertProductStatement = connection.prepareStatement(insertProductQuery, Statement.RETURN_GENERATED_KEYS);
-                 PreparedStatement insertPriceHistoryStatement = connection.prepareStatement(insertPriceHistoryQuery)) {
+                 PreparedStatement insertPriceHistoryStatement = connection.prepareStatement(insertPriceHistoryQuery);
+                 PreparedStatement checkExistingProductStatement = connection.prepareStatement(checkExistingProductQuery)) {
 
                 for (Product product : products) {
-                    // Insertar en la tabla Products
-                    insertProductStatement.setInt(1, product.getId());
-                    insertProductStatement.setInt(2, idSupermarket);
-                    insertProductStatement.setString(3, product.getName());
-                    insertProductStatement.setDouble(4, product.getPrice());
-                    insertProductStatement.setString(5, product.getImageUrl());
-                    insertProductStatement.setString(6, product.getUrlProduct());
+                    // Verificar si ya existe un producto con los mismos id_by_supermarket y id_supermarket
+                    checkExistingProductStatement.setInt(1, product.getId());
+                    checkExistingProductStatement.setInt(2, idSupermarket);
+                    ResultSet existingProductResult = checkExistingProductStatement.executeQuery();
+                    existingProductResult.next();
+                    int existingProductCount = existingProductResult.getInt(1);
 
-                    int affectedRows = insertProductStatement.executeUpdate();
+                    if (existingProductCount == 0) {
+                        // No existe, proceder con la inserción
+                        // Insertar en la tabla Products
+                        insertProductStatement.setInt(1, product.getId());
+                        insertProductStatement.setInt(2, idSupermarket);
+                        insertProductStatement.setString(3, product.getName());
+                        insertProductStatement.setDouble(4, product.getPrice());
+                        insertProductStatement.setString(5, product.getImageUrl());
+                        insertProductStatement.setString(6, product.getUrlProduct());
 
-                    if (affectedRows == 0) {
-                        throw new SQLException("La inserción del producto falló, no se generaron claves.");
+                        int affectedRows = insertProductStatement.executeUpdate();
+
+                        if (affectedRows == 0) {
+                            throw new SQLException("La inserción del producto falló, no se generaron claves.");
+                        }
+
+//                    } else {
+//                        // Ya existe, puedes manejar esto de alguna manera si es necesario
+//                        System.out.println("El producto ya existe en la base de datos.");
+//                    }
+//                        connection.commit(); // Confirmar transacción
                     }
-
                     // Consulta para obtener el ID del producto recién insertado
-                    String getIdQuery = "SELECT last_insert_rowid() AS id";
-                    try (PreparedStatement getIdStatement = connection.prepareStatement(getIdQuery);
-                         ResultSet idResult = getIdStatement.executeQuery()) {
+                    String getIdQuery = "SELECT id FROM Products WHERE id_by_supermarket = ? AND id_supermarket = ?";
 
+                    try (PreparedStatement getIdStatement = connection.prepareStatement(getIdQuery)) {
+                        getIdStatement.setInt(1, product.getId());
+                        getIdStatement.setInt(2, idSupermarket);
+                        ResultSet idResult = getIdStatement.executeQuery();
                         if (idResult.next()) {
                             int productId = idResult.getInt("id");
 
@@ -69,12 +89,49 @@ public class DatabaseManager {
                             insertPriceHistoryStatement.setInt(1, productId);
                             insertPriceHistoryStatement.setDouble(2, product.getPrice());
                             insertPriceHistoryStatement.executeUpdate();
+
+                            // Actualizar el precio del producto en la tabla Products
+                            String updateProductPriceQuery = "UPDATE Products SET price = ? WHERE id = ?";
+                            try (PreparedStatement updateProductPriceStatement = connection.prepareStatement(updateProductPriceQuery)) {
+                                updateProductPriceStatement.setDouble(1, product.getPrice());
+                                updateProductPriceStatement.setInt(2, productId);
+                                updateProductPriceStatement.executeUpdate();
+                            }
                         } else {
                             throw new SQLException("La consulta para obtener el ID del producto falló.");
                         }
+                        connection.commit();
+                    } catch (SQLException e) {
+                        // En caso de error, hacer rollback
+                        if (connection != null) {
+                            try {
+                                connection.rollback();
+                            } catch (SQLException rollbackException) {
+                                rollbackException.printStackTrace();
+                            }
+                        }
+                        e.printStackTrace();
                     }
                 }
-                connection.commit(); // Confirmar transacción
+            } catch (SQLException e) {
+                // En caso de error, hacer rollback
+                if (connection != null) {
+                    try {
+                        connection.rollback();
+                    } catch (SQLException rollbackException) {
+                        rollbackException.printStackTrace();
+                    }
+                }
+                e.printStackTrace();
+            } finally {
+                // Cerrar la conexión en el bloque finally
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (SQLException closeException) {
+                        closeException.printStackTrace();
+                    }
+                }
             }
         } catch (SQLException e) {
             // En caso de error, hacer rollback
