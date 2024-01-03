2d5cfe99fb79740d965e1751121e9104aa043d76
Dario Afonso <alu0100406580@ull.edu.es>
2024-01-03 00:47:17 +0000

    copia BD desde target, no reinicio spring, mejoras listaProductos Mercadona

--- src/main/java/ull/es/supermarketmvc/controller/MercadonaController.java
+++ src/main/java/ull/es/supermarketmvc/controller/MercadonaController.java
@@ -1,6 +1,7 @@
 package ull.es.supermarketmvc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,10 @@ import ull.es.supermarketmvc.model.Product;
 import ull.es.supermarketmvc.repository.PriceHistoryRepository;
 import ull.es.supermarketmvc.repository.ProductRepository;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class MercadonaController {
@@ -26,22 +30,37 @@ public class MercadonaController {
         // Obtener la lista de productos desde el repositorio
 //        Iterable<Product> productos = productRepository.findAll();
         List<Product> products = productRepository.findProductBySupermarketId(2);
+        List<Long> productsIds = products.stream().map(Product::getId).collect(Collectors.toList());
+        LocalDate dateDatabaseUpdate = priceHistoryRepository.findMaxDateByProductIds(productsIds);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = dateDatabaseUpdate.format(formatter);
 
         model.addAttribute("products", products);
         model.addAttribute("supermarketName", supermarketName);
+        model.addAttribute("dateDatabaseUpdate", formattedDate);
 
-        return "listaProductos";
+        return "productList";
     }
 
-    @GetMapping("/mercadona/update")  // Cambia "ejecutar-crawler" por la ruta que desees
-    public String executeCrawler() {
+//    @GetMapping("/mercadona/update")  // Cambia "ejecutar-crawler" por la ruta que desees
+//    public String executeCrawler() {
+//        System.out.println("Inicio actualizacion BD");
+//        long startTime = System.currentTimeMillis();
+//        MercadonaCrawler crawlerMercadona = new MercadonaCrawler();
+//        long mercadonaTime = System.currentTimeMillis() - startTime;
+//        System.out.println("Tiempo de ejecución de MercadonaCrawler: " + mercadonaTime / 1000 + " segundos");
+//
+//        // Puedes retornar una vista o redirigir a otra URL según tus necesidades
+//        return "productList";  // Cambia "vista-resultados" por el nombre de tu vista
+//    }
+    @GetMapping("/mercadona/update")
+    public ResponseEntity<String> executeCrawler() {
         System.out.println("Inicio actualizacion BD");
         long startTime = System.currentTimeMillis();
         MercadonaCrawler crawlerMercadona = new MercadonaCrawler();
         long mercadonaTime = System.currentTimeMillis() - startTime;
         System.out.println("Tiempo de ejecución de MercadonaCrawler: " + mercadonaTime / 1000 + " segundos");
 
-        // Puedes retornar una vista o redirigir a otra URL según tus necesidades
-        return "listaProductos";  // Cambia "vista-resultados" por el nombre de tu vista
+        return ResponseEntity.ok("Actualización exitosa"); // Cambia esto según la lógica de tu aplicación
     }
 }
